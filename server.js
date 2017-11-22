2b2d38b6f973688e8ee77745512df0866a26cda5
philip fraser <pr.fraser3@gmail.com>
2017-11-22 11:11:46 +1000

    setup express basic server

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+let students = ['Scott', 'Simon'];
+
+app.set('view engine', 'pug');
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
+
+app.get('/api/students', (req, res) => {
+  res.send(students);
+});
+
+app.get('/students', (req, res) => {
+  res.send(students);
+});
+
+app.post('/students', (req, res) => {
+  let student = req.body.name;
+  students.push(student);
+  res.send(student);
+});
+
+app.listen(port);
